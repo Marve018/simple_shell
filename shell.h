e7bfb4f942faf481e8784129a648fb2654b2d7cb
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-04-22 18:05:07 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* fun_list */
+int _putchar(char c);
+int _atoi(char *s);
+int _strlen(const char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+
+/* fun_list2 */
+int _strncmp(const char *s1, const char *s2, size_t len);
+char *_strdup(char *str);
+void _puts(char *str);
+void *_realloc(void *ptr, unsigned int o_size, unsigned int n_size);
+
+#endif
