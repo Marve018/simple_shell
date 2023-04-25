b8c67ccd851b551e69941588441106e81f6a9f3b
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-04-25 11:41:24 -0700

    adding the environment file containing new functions

---
+++ environ.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+  * _getenv - Gets environment variable
+  * @name: The variable to find in the system environment
+  *
+  * Return: The content of the environment variable
+  */
+char *_getenv(const char *name)
+{
+	char *e_var = NULL;
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		if (_strncmp(environ[i], name, _strlen(name)) == 0)
+		{
+			e_var = environ[i] + _strlen(name) + 1;
+			return (e_var);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+  * _print_env - Prints the environment builtin
+  *
+  * Return: void
+  */
+void _print_env(void)
+{
+	int i = 0, j = 0;
+
+	while (environ[i] != NULL)
+	{
+		j = 0;
+		while (environ[i][j])
+		{
+			_putchar(environ[i][j]);
+			j++;
+		}
+		if (j != 0)
+			_putchar('\n');
+		i++;
+	}
+}
