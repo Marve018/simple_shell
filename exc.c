c219a6d29c6611e259ba623ec5628dc135ffe038
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-04-25 08:54:27 -0700

    included new countdelimiter function

--- exc.c
+++ exc.c
@@ -33,3 +33,25 @@ int exec(char *cmd, char **copts)
 	}
 	return (0);
 }
+
+/**
+  * count_delims - Counts delimiters in a string
+  * @str: The string that contains the delimiters
+  * @delim: The delimiters to will find in the string
+  *
+  * Return: The number of delimiters in the string
+  */
+int count_delims(char *str, char *delim)
+{
+	int cwrd = 0, i, j;
+
+	for (i = 0; delim[i]; i++)
+	{
+		for (j = 0; str[j]; j++)
+		{
+			if (str[j] == delim[i])
+				cwrd++;
+		}
+	}
+	return (cwrd);
+}
